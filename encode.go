22506252466c29ae22dbcce8541fafa624cce2cb
Liu Ming <extrafliu@gmail.com>
2015-09-24 11:55:12 +0800

    add multi value support

--- encode.go
+++ encode.go
@@ -21,9 +21,9 @@ type OpenTsdbEncoderConfig struct {
 	//json decode message use Tags config and Value config
 	//bson decode as json but treat payload as bson object
 	//raw decode value from payload and tags from message
-	Decode   string
-	Tags_str string `toml:"tags"`
-	Value    string
+	Decode string
+	Tags   []string
+	Values []string
 	//the metric root
 	Metric string
 }
@@ -46,44 +46,52 @@ func (e *OpenTsdbEncoder) Encode(pack *PipelinePack) (output []byte, err error) 
 	payload := pack.Message.GetPayload()
 	tagMsg := make(map[string]interface{})
 
-	var dp opentsdb.DataPoint
+	dp := make([]*opentsdb.DataPoint, len(e.Values))
 
 	switch e.OpenTsdbEncoderConfig.Decode {
 	case "json":
 		if err = json.Unmarshal([]byte(payload), &tagMsg); err != nil {
 			return nil, err
-		} else {
-			dp.Value = tagMsg[e.Value]
-			for _, v := range e.Tags {
-				if tag, ok := tagMsg[v]; ok {
-					dp.Tags[v] = fmt.Sprintf("%v", tag)
-				}
-			}
 		}
+		goto j_b_son_get_value
 	case "bson":
 		if err = bson.Unmarshal([]byte(payload), &tagMsg); err != nil {
 			return nil, err
-		} else {
-			dp.Value = tagMsg[e.Value]
-			for _, v := range e.Tags {
-				if tag, ok := tagMsg[v]; ok {
-					dp.Tags[v] = fmt.Sprintf("%v", tag)
-				}
-			}
 		}
+		goto j_b_son_get_value
 	default:
-		dp.Value = payload
-		for _, v := range e.Tags {
-			if tag, ok := pack.Message.GetFieldValue(v); ok {
-				dp.Tags[v] = fmt.Sprintf("%v", tag)
+		for i := 0; i < len(e.Values); i++ {
+			if e.Values[i] == "payload" {
+				dp[i].Value = payload
+				dp[i].Metric = e.Metric
+			} else {
+				dp[i].Value = pack.Message.GetFieldValue(e.Values[i])
+				dp[i].Metric = e.Metric + "." + e.Values[i]
 			}
 		}
+		goto copy_0_tags_to_n
 	}
 
-	dp.Metric = e.Metric
-	dp.Timestamp = time.Now().Unix()
+j_b_son_get_value:
+	for i := 0; i < len(e.Values); i++ {
+		dp[i].Value = tagMsg[e.Values[i]]
+		dp[i].Metric = e.Metric + "." + e.Values[i]
+	}
+	for _, v := range e.Tags {
+		if tag, ok := tagMsg[v]; ok {
+			dp[0].Tags[v] = fmt.Sprintf("%v", tag)
+		}
+	}
+copy_0_tags_to_n:
+	for i := 0; i < len(dp); i++ {
+		dp[i].Tags = dp[0].Tags
+		dp[i].Timestamp = time.Now().Unix()
+	}
 
-	return json.Marshal(dp)
+	bts := json.Marshal(dp)
+	bts = bts[1:]
+	bts = bts[:len(bts)-1]
+	return bts
 }
 
 func init() {
